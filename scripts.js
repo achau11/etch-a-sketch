02e34ebdbcfb9ce7b0682333f324da0e61be82ed
xazzer81 <achau1019@gmail.com>
2020-01-02 21:58:23 -0500

    Added color change, button to create new grid, CSS styles

--- scripts.js
+++ scripts.js
@@ -1,12 +1,37 @@
+// notes for me: read more on foreach, css grid.
+
 const container = document.querySelector('.container');
-// Create 16x16 Divs:
+// function to create sizexsize grid:
 function createGrid(size){
     for (i = 0; i < size; i++){
         for (x = 0; x < size; x++){
-            const gridBox = document.createElement('div');
-            gridBox.textContent = "I'm a box";
+            let gridBox = document.createElement('div');
             container.appendChild(gridBox);
         }   
     }
 }
-createGrid(16);
+// function to change color of div on hover:
+function changeColor(e){
+    e.target.classList.add('hover');
+}
+// start
+createGrid(16);  
+
+// add event listener to each div in grid
+const divs = document.querySelectorAll('.container div');
+divs.forEach( function(divs){
+    divs.addEventListener('mouseover', changeColor);
+});               
+//function to revert color of all divs:
+function revertColor(){
+    divs.forEach(function(divs){
+        divs.classList.remove('hover');
+    });
+}
+// prompt user for squares on button click and make grid of size
+const button = document.querySelector('button');
+button.addEventListener('click', function(e){
+    const numOfSquares = prompt("How many squares per side for new grid?");
+    createGrid(numOfSquares);
+    revertColor();
+});
